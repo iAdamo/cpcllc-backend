845ef3834092c2e51dbccff0ee9ff7a040183d1a
Adam_Babatunde_Sanusi <tundey520@gmail.com>
2025-02-22 10:38:16 +0100

    creating users controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UsersService } from '@services/users.service';
+import { CreateAdminDto } from '@dto/create-admin.dto';
+import { CreateClientDto } from '@dto/create-client.dto';
+import { CreateCompanyDto } from '@dto/create-company.dto';
 
+@ApiTags('Users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('admin')
+  async createAdmin(@Body() createAdminDto: CreateAdminDto) {
+    return this.usersService.createAdmin(createAdminDto);
+  }
+
+  @Post('client')
+  async createClient(@Body() createClientDto: CreateClientDto) {
+    return this.usersService.createClient(createClientDto);
+  }
+
+  @Post('company')
+  async createCompany(@Body() createCompanyDto: CreateCompanyDto) {
+    return this.usersService.createCompany(createCompanyDto);
+  }
+}
